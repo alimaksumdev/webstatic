ac8e390bf9283b855dbd4e98072a082bc7257cdc
zaynhakim <zaynhakim@gmail.com>
2022-03-08 22:15:00 +0700

    minor edit

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 const Intro = () => {
   return (
     <section className="flex min-h-screen flex-col items-center justify-center py-2 bg-[url('/assets/images/intro-image.jpg')] bg-cover bg-center">
-      <h1 className="text-center text-3xl md:text-6xl lg:text-8xl font-bold text-sky-50 tracking-tighter leading-tight drop-shadow-md px-8">
+      <h1 className="text-center text-3xl md:text-6xl lg:text-8xl font-bold text-sky-50 tracking-tighter leading-tight drop-shadow-md md:px-8">
         SMP-SMA Ali Maksum
       </h1>
-      <h4 className="text-center text-sm md:text-lg lg:text-xl text-slate-50 bg-blend-difference tracking-tighter leading-tight drop-shadow-sm px-8">
+      <h4 className="text-center text-sm md:text-lg lg:text-xl text-slate-50 tracking-tight leading-tight drop-shadow-md px-2">
         Membentuk insan beriman kuat, taqwa yang kaafah, berakhlaq mulia,
         menguasai IPTEK yang mashlahah, beramal profesional.
       </h4>
-      <div className="flex py-4">
+      <div className="columns-2 flex-col my-2">
       <button
         id="navAction"
-        className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white hover:text-slate-100 no-underline font-semibold h-12 px-4 mx-2 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+        className="mx-auto min-w-full px-2 bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white hover:text-slate-100 no-underline font-semibold h-12 rounded-lg flex items-center justify-center dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
       >
         <Link href={'/posts/info-pendaftaran'}>Info Pendaftaran</Link>
       </button>
       <button
         id="navAction"
-        className="bg-slate-100 hover:bg-slate-50 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-slate-900 hover:text-slate-500 no-underline font-semibold h-12 px-4 mx-2 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-slate-900 dark:highlight-white/20 dark:hover:bg-slate-700 dark:text-slate-100 dark:hover:text-slate-50"
+        className="bg-mx-auto min-w-full px-2 slate-100 hover:bg-slate-50 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-slate-900 hover:text-slate-500 no-underline font-semibold h-12 rounded-lg flex items-center justify-center dark:bg-slate-900 dark:highlight-white/20 dark:hover:bg-slate-700 dark:text-slate-100 dark:hover:text-slate-50"
       >
         <Link href={'/posts/about'}>Profil</Link>
       </button>
